874de33a159204ba55c761db9e2b2536a6576620
braem <itbing@sina.cn>
2019-09-29 14:31:04 +0800

    add application migration

---
+++ database/migrations/2019_09_29_062522_create_application_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('application', function (Blueprint $table) {
+            $table->string('aid')->unique()->comment('应用标识');
+            $table->integer('seat_num')->comment('坐席数');
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('application');
+    }
+}
