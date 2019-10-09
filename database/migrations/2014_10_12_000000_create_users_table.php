b1b5cd0a459362b6a83ee6d32eeddeb2d30c6f93
braem <itbing@sina.cn>
2019-10-09 17:34:01 +0800

    users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('true_name',60)->comment('真实姓名');
+            $table->string('job_num',60)->comment('工号');
+            $table->string('email',60)->unique()->comment('邮箱');
+            $table->string('phone',30)->unique()->comment('手机号');
+            $table->string('password')->comment('密码');
+            $table->tinyInteger('role',false,false)->comment('角色 1:超管 0:普通客服');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
