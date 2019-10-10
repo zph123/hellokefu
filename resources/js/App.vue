9e99c630206b804735dedbdd3ea2447ef2ba23cb
braem <itbing@sina.cn>
2019-10-10 16:42:02 +0800

    updaet aside page

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,68 +1,69 @@
 <template>
-  <div id="app">
-      <el-container>
-          <el-header>
-              <Header></Header>
-          </el-header>
-          <el-container>
-              <el-aside width="200px">
-                  <Aside></Aside>
-              </el-aside>
-              <el-main><router-view/></el-main>
-          </el-container>
-      </el-container>
-  </div>
+    <div id="app">
+        <el-container>
+            <el-header>
+                <Header></Header>
+            </el-header>
+            <el-container>
+                <Aside></Aside>
+                <el-main>
+                    <router-view/>
+                </el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 <script>
-  import Header from './components/Header.vue'
-  import Aside from './components/Aside.vue'
+    import Header from './components/Header.vue'
+    import Aside from './components/Aside.vue'
 
-  export default {
-    name: 'app',
-    data () {
-      return {
-        visible: false,
-      }
-    },
-    components: {
-      Header,
-      Aside
-    }
-  };
+    export default {
+        name: 'app',
+        data() {
+            return {
+                visible: false,
+            }
+        },
+        components: {
+            Header,
+            Aside
+        }
+    };
 </script>
 <style scoped>
-  /* Add scoped styles */
-  .el-header, .el-footer {
-      background-color: #B3C0D1;
-      color: #333;
-      text-align: center;
-      line-height: 60px;
-  }
+    /* Add scoped styles */
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
 
-  .el-aside {
-      background-color: #D3DCE6;
-      color: #333;
-      text-align: center;
-      /*line-height: 200px;*/
-  }
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+        text-align: center;
+        line-height: 200px;
+        height: 100%;
+    }
 
-  .el-main {
-      background-color: #E9EEF3;
-      color: #333;
-      text-align: center;
-      /*line-height: 160px;*/
-  }
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+        text-align: center;
+        /*line-height: 160px;*/
+    }
 
-  body > .el-container {
-      margin-bottom: 40px;
-  }
+    body > .el-container {
+        margin-bottom: 40px;
+    }
 
-  .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-      line-height: 260px;
-  }
+    .el-container:nth-child(5) .el-aside,
+    .el-container:nth-child(6) .el-aside {
+        line-height: 260px;
+    }
 
-  .el-container:nth-child(7) .el-aside {
-      line-height: 320px;
-  }
+    .el-container:nth-child(7) .el-aside {
+        line-height: 320px;
+    }
 </style>
--- resources/js/components/Aside.vue
+++ resources/js/components/Aside.vue
@@ -10,30 +10,22 @@
                     text-color="#fff"
                     active-text-color="#ffd04b">
                 <el-submenu index="1">
-                    <template slot="title">
-                        <i class="el-icon-location"></i>
-                        <span>导航一</span>
-                    </template>
-                    <el-menu-item-group>
-                        <template slot="title">分组一</template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="分组2">
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-submenu index="1-4">
-                        <template slot="title">选项4</template>
-                        <el-menu-item index="1-4-1">选项1</el-menu-item>
-                    </el-submenu>
+                    <template slot="title"><i class="el-icon-location"></i>主页</template>
+                    <el-menu-item index="2-1">选项1</el-menu-item>
+                    <el-menu-item index="2-2">选项2</el-menu-item>
+                    <el-menu-item index="2-3">选项3</el-menu-item>
                 </el-submenu>
                 <el-menu-item index="2">
                     <i class="el-icon-menu"></i>
-                    <span slot="title">导航二</span>
+                    <span slot="title">会话</span>
                 </el-menu-item>
                 <el-menu-item index="3">
                     <i class="el-icon-setting"></i>
-                    <span slot="title">导航三</span>
+                    <span slot="title">留言</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                    <i class="el-icon-setting"></i>
+                    <span slot="title">设置</span>
                 </el-menu-item>
             </el-menu>
         </el-aside>
@@ -42,6 +34,14 @@
 
 <script>
     export default {
+        methods: {
+            handleOpen(key, keyPath) {
+                console.log(key, keyPath);
+            },
+            handleClose(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        },
         data() {
             return {}
         }
