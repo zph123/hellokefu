08390189854704c0be60e695a55eeee09d51fd15
braem <itbing@sina.cn>
2019-10-16 09:46:24 +0800

    add icons

--- resources/js/utils/validate.js
+++ resources/js/utils/validate.js
@@ -8,3 +8,7 @@
 export function checkEmail(email) {
     return /^[a-z]([a-zA-Z0-9_]{5,20})@([a-z0-9]{1,10})([.]{1})([a-z]{2,4})$/.test(email)
 }
+
+export function isExternal(path) {
+    return /^(https?:|mailto:|tel:)/.test(path)
+}
